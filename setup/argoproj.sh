402ebaefb139c48f0349476fbf2dd8794ad5e5db
chinbe73 <bernhard.ineichen@gmail.com>
2020-08-29 21:53:32 +0000

    krew

--- setup/argoproj.sh
+++ setup/argoproj.sh
@@ -1,4 +1,3 @@
-
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v1.7.2/manifests/install.yaml
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
@@ -8,9 +7,9 @@ kubectl apply -n argo-rollouts -f https://raw.githubusercontent.com/argoproj/arg
 
 ADMIN_PASSWORD=admin
 watch kubectl -n argocd get deploy
-argocd --port-forward login --username admin --password ${ADMIN_PASSWORD}
+argocd --port-forward login --username admin --password $ADMIN_PASSWORD
 if [ $? -ne 0 ]; then
     argocd --port-forward login --username admin --password $(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2)
-    argocd --port-forward account update-password --current-password $(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2) --new-password ${ADMIN_PASSWORD}
+    argocd --port-forward account update-password --current-password $(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2) --new-password $ADMIN_PASSWORD
     argocd --port-forward login --username admin --password admin
 fi
