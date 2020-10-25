a9402340919e61252c24155b432f8e74fa2aaf5c
Bernhard Ineichen <bernhard.ineichen@visana.ch>
2020-10-25 23:02:11 +0100

    repair

--- vagrant/bash/provision.sh
+++ vagrant/bash/provision.sh
@@ -8,8 +8,6 @@ kubectl completion bash > /etc/profile.d/kubectl_completion.sh
 # Install oc
 curl -Lso /dev/stdout https://github.com/openshift/okd/releases/download/4.5.0-0.okd-2020-10-15-235428/openshift-client-linux-4.5.0-0.okd-2020-10-15-235428.tar.gz | tar xz -C /usr/bin oc
 oc completion bash > /etc/profile.d/oc_completion.sh
-# Upgrade second time (needed?)
-#apt upgrade -y
 # Configure user home of user vagrant
 cat | sudo -u vagrant bash <<'EOF1'
 # Link folders
@@ -20,11 +18,11 @@ if [ -f ~/Home/.ssh/id_rsa.pub ]; then cat ~/Home/.ssh/id_rsa.pub >> ~/.ssh/auth
 # Disable background color for other-writable folders
 dircolors -p | sed "s/^STICKY_OTHER_WRITABLE 30\;42/STICKY_OTHER_WRITABLE 30\;44/;s/^OTHER_WRITABLE 34\;42/OTHER_WRITABLE 34\;40/" > ~/.dircolors
 # Disable welcome message
-#touch ~/.hushlogin
+touch ~/.hushlogin
 # Create bash_aliases (aliases, functions and more)
 cat > ~/.bash_aliases <<'EOF2'
 alias k='kubectl'
-alias wd='if [ -f ~/Home/.dir ]; then cd $(cat ~/Home/.dir | sed s@~@$HOME@); fi'
+alias wd='if [ -f ~/Home/.workdir ]; then cd $(cat ~/Home/.workdir | sed s@~@$HOME@); fi'
 complete -F __start_kubectl k
 function kns () { kubectl config set-context --current --namespace=\$1; }
 if [ -f ~/Home/.bash_profile ]; then . ~/Home/.bash_profile; fi
